ef34a30b49e0ab79b342cc25a216c68a1e953d2b
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-02-20 22:14:08 -0500

    connected back and front

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   res.send('Welcome to the ProgressFit backend');
 });
 
