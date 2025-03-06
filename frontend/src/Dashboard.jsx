05a4875230eddac7ce31affeb129af5f21cf703f
RomannaBidnyk <romanna.bidnyk@gmail.com>
2025-03-06 17:35:21 -0500

    updated dashboard

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import styles from "./Dashboard.module.css"; // Import the styles
 
 const Dashboard = ({ user, setUser }) => {
   const navigate = useNavigate();
@@ -10,20 +11,24 @@ const Dashboard = ({ user, setUser }) => {
   };
 
   return (
-    <div className="dashboard">
+    <div className={styles.dashboard}>
       <h2>Welcome, {user.name}!</h2>
-      <div className="dashboard-buttons">
-        <button className="btn" onClick={() => navigate("/weight")}>
+      <p className={styles.description}>
+        Track your progress by logging your weight and food intake. Use the
+        buttons below to navigate to different sections.
+      </p>
+      <div className={styles.dashboardButtons}>
+        <button className={styles.btn} onClick={() => navigate("/weight")}>
           Weight
         </button>
-        <button className="btn" onClick={() => navigate("/food")}>
+        <button className={styles.btn} onClick={() => navigate("/food")}>
           Food
         </button>
       </div>
       <br />
-      <button className="btn" onClick={handleLogout}>
+      <a href="#" className={styles.logoutLink} onClick={handleLogout}>
         Logout
-      </button>
+      </a>
     </div>
   );
 };
